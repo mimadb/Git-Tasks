65501d85df8210ccb34d340b1f3e2b55a50f53ff
mimadb <68076122+mimadb@users.noreply.github.com>
2021-06-12 13:37:02 -0500

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,72 @@
+#TASK THREE
+#No. 1: Create a list of 10 elements of four different data types like int, string, complex and float.
+
+mylist = [1,2.5,3+2j,"Consultadd",5,3.5,9+3j,5,5,5]
+
+#%%No.2 
+#Create a list of size 5 and execute the slicing structure
+mylist = [0,1,2,3,4]
+print(mylist[:2])
+print(mylist[2:])
+print(mylist[2:4])
+print(mylist[0:5:2])
+
+#%%No.3
+#Write a program to get the sum and product of all the items in a given list.
+mylist = [1,2,3,4,5]
+
+sum = sum(mylist)
+
+prod = 1
+for i in mylist:
+    prod = prod * i
+
+#%% No.4
+#Find the largest and smallest number from a given list.
+
+mylist = [5,2,8,123,4,-2,56,21,-104]
+
+mymin = min(mylist)
+mymax = max(mylist)
+
+#%% No.5
+# Create a new list which contains the specified numbers after removing the even numbers from a
+#predefined list.
+
+mylist = [1,2,3,4,5,6,7,8,22,33,44,55,66]
+
+for i in mylist:
+    if i % 2 == 0:
+        mylist.remove(i)
+        
+#%% No.6
+#Create a list of elements such that it contains the squares of the first and last 5 elements between
+#1 and30 (both included).
+mylist = list(range(1,6))+list(range(25,31))
+mylist = list(map(lambda x:x**2, mylist))
+
+#%% No.7
+#Write a program to replace the last element in a list with another list.
+mylist = [1,2,3,4,5,6]
+listtoadd = ['a','b','c']
+
+mylist.pop(-1)
+mylist += listtoadd
+#%%  No.8
+#Create a new dictionary by concatenating the following two dictionaries:
+a = {1:10,2:20}
+b = {3:30,4:40}
+newdict = dict(a)
+newdict.update(b)
+#%%  No.9
+#Create a dictionary that contain numbers in the form(x:x*x) where x takes all the values between 1
+#and n(both 1 and n included).
+n = int(input("Enter the value of n: "))
+mydict = {i:i*i for i in range(1,n+1)}
+
+#%% No.10
+#Write a program which accepts a sequence of comma-separated numbers from console and
+#generates a list and a tuple which contains every number in the form of string.
+numbers = input("Write down your numbers (comma sep): ")
+mylist = numbers.split(",")
+mytuple = tuple(mylist)
