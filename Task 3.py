0ad6c9cb008c5b757fee74020ab3a947fabd9182
mimadb <68076122+mimadb@users.noreply.github.com>
2021-06-14 10:31:38 -0500

    Add files via upload
    Added eval(input()) for exercises which stated "Sample Input" after clarification from Bhumika

--- Task 3.py
+++ Task 3.py
@@ -13,27 +13,31 @@ print(mylist[0:5:2])
 
 #%%No.3
 #Write a program to get the sum and product of all the items in a given list.
-mylist = [1,2,3,4,5]
+mylist = eval(input("Write your list:"))
+#mylist = [1,2,3,4,5]
 
-sum = sum(mylist)
-
+mysum = sum(mylist)
+print("Sum:",mysum)
 prod = 1
 for i in mylist:
     prod = prod * i
-
+print("Product", prod)
 #%% No.4
 #Find the largest and smallest number from a given list.
 
-mylist = [5,2,8,123,4,-2,56,21,-104]
+#mylist = [5,2,8,123,4,-2,56,21,-104]
+mylist = eval(input("Write your list:"))
 
 mymin = min(mylist)
 mymax = max(mylist)
+print("Min:",mymin,"\n","Max:",mymax)
 
 #%% No.5
 # Create a new list which contains the specified numbers after removing the even numbers from a
 #predefined list.
 
-mylist = [1,2,3,4,5,6,7,8,22,33,44,55,66]
+mylist = eval(input("Write your list:"))
+#mylist = [1,2,3,4,5,6,7,8,22,33,44,55,66]
 
 for i in mylist:
     if i % 2 == 0:
@@ -44,18 +48,22 @@ for i in mylist:
 #1 and30 (both included).
 mylist = list(range(1,6))+list(range(25,31))
 mylist = list(map(lambda x:x**2, mylist))
+print(mylist)
 
 #%% No.7
 #Write a program to replace the last element in a list with another list.
-mylist = [1,2,3,4,5,6]
-listtoadd = ['a','b','c']
+mylist = eval(input("Write your first list:"))
+listtoadd = eval(input("Write your second list:"))
 
 mylist.pop(-1)
 mylist += listtoadd
+print(mylist)
 #%%  No.8
 #Create a new dictionary by concatenating the following two dictionaries:
-a = {1:10,2:20}
-b = {3:30,4:40}
+a = eval(input("Write your first dictionary:"))
+b = eval(input("Write your second dictionary:"))
+#a = {1:10,2:20}
+#b = {3:30,4:40}
 newdict = dict(a)
 newdict.update(b)
 #%%  No.9
